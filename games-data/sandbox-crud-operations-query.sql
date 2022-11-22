3522aff93ed7e78b3771fc08cac5f5517c435ae3
Jose Garza <jjman46@hotmail.com>
2022-11-22 16:00:15 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int16_value, string_value, datetime_value) VALUES (2098, 'Pizza bby', '2008-06-02 10:10:22');
+
+--UPDATE sandbox SET int32_value = -1092784 WHERE id = 90;
+
+--DELETE FROM sandbox WHERE id = 77;
+
+SELECT * FROM sandbox;
